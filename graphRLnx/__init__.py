940b1a2559c4cd83f53cc29790ededcd0d8c3634
luludarcy <luludarcy@gmail.com>
2018-12-15 20:12:25 +0000

    final renaming

--- graphRLnx/__init__.py
+++ graphRLnx/__init__.py
@@ -2,6 +2,6 @@ from gym.envs.registration import register
 
 register(
     id='graphRL-v0',
-    entry_point='graphRLnX.envs:graphRLnX',
+    entry_point='graphRLnx.envs:graphRLnx',
     kwargs={'network_size': 10, 'input_nodes': 3}
 )
