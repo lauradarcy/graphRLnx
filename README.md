ec88c2b081d598d462dd9ca5454597552d69827e
luludarcy <luludarcy@gmail.com>
2018-12-15 16:25:46 +0000

    new repo

---
+++ README.md
@@ -0,0 +1,28 @@
+# graphRL with NetworkX
+
+[![PyPI version](https://badge.fury.io/py/graphRL.svg)](https://badge.fury.io/py/graphRL)
+
+This repository contains a PIP package which is an OpenAI environment for simulating directed acyclic graphs for workflow scheduling, using a NetworkX backend.
+
+
+## Installation
+
+install this package via
+
+```bash
+pip3 install graphRLnX
+```
+
+## Usage
+
+```python
+import gym
+import graphRLnX
+
+env = gym.make('graphRL-v0')
+```
+
+
+## The Environment
+
+Tbc.
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(name='graphRLnX',
+	version='0.0.1',
+	author='Laura D\'Arcy',
+	author_email='luludarcy@gmail.com',
+	description='a directed acyclic graph environment for use with openAI gym, with a networkX backend',
+	long_description=long_description,
+	long_description_content_type='text/markdown',
+	url='https://github.com/luludarcy/graphRLnX',
+	packages=setuptools.find_packages(),
+	install_requires=['gym', 'networkX'],  # And any other dependencies graphRL needs
+	classifiers=[
+		"Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
